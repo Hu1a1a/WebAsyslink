42dba08e67c213a19756e61e6859325e8a9e91bf
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2023-12-17 18:41:52 +0100

    Acceso Cliente y admin

---
+++ javascript/accesoAdmin.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $.ajax({
+        url: './php/acceso.php',
+        type: 'post',
+        success: function(result) {
+            if(result != "admin") {
+                var target = document.querySelector('body');
+                target.style.backgroundImage = "none";
+                var message = "<h1>Error 404: Not found</h1>";
+                target.innerHTML = message;
+            }
+        }
+    });
+});
