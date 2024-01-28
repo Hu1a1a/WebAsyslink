53089714236cc3626f7bd6b31dfae6454355bab3
Hulala <yang.ye.1@hotmail.com>
2024-01-28 21:40:07 +0100

    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,14 +1,26 @@
 <?php
-    // Datos de conexion
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "mydb";
-    $port = 3310 ;
-    
-    // Conectar a la base de datos
-    $mysqli = mysqli_connect($host, $user, $password, $database, $port);
-    if ($mysqli->connect_errno) {
-        echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "<br/>";
-    }
-?>
+// Datos de conexion
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "mydb";
+$port = 3310;
+
+// Conectar a la base de datos
+$mysqli = mysqli_connect($host, $user, $password, $database, $port);
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "<br/>";
+}
+/*
+// Datos de conexion
+$host = "db5015081195.hosting-data.io";
+$user = "dbu4778339";
+$password = "dhsdugfduygvuygu547835678";
+$database = "dbs12522649";
+
+// Conectar a la base de datos
+$mysqli = mysqli_connect($host, $user, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "<br/>";
+}
+*/
